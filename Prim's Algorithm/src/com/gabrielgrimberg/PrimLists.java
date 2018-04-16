e676ed3896dd9ee48ab9718f110b5106d075a2f4
GabrielGrimberg <gabby.gr97@hotmail.com>
2018-04-16 22:07:01 +0100

    Prim's Algorithm - IDE Structure

--- Prim's Algorithm/src/com/gabrielgrimberg/PrimLists.java
+++ Prim's Algorithm/src/com/gabrielgrimberg/PrimLists.java
@@ -1,4 +1,6 @@
-/* 
+package com.gabrielgrimberg;
+
+/*
     Name: Gabriel Grimberg.
     Course: DT228/2.
     Module: Algorithms & Data Structures.
@@ -11,24 +13,24 @@ import java.io.*;
 import java.util.Scanner;
 
 public class PrimLists {
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         Scanner keyInput = new Scanner(System.in);
-        
+
         // Entering the file name.
         System.out.println("Enter the name of the file including the .txt : ");
-        String fname = keyInput.nextLine(); 
-        
+        String fname = keyInput.nextLine();
+
         // Entering the starting vertex.
         System.out.println("Enter the vertex you wish to start on : ");
-        int startVertex = keyInput.nextInt();             
-        
+        int startVertex = keyInput.nextInt();
+
         Graph g = new Graph(fname);
-       
+
         g.display();                   // Display the graph.
         g.MST_Prim(startVertex);       // Performe Algorithm and display the weight of it.
         g.showMST();                   // Show the MST.
-               
+
     }
 }
